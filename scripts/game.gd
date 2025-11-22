7bda98fe2a14adaefff4cf9773a591fb4636a8d3
ToasterPanic <me@owouw.us>
2025-11-22 16:24:30 -0500

    Sound effects, more textures, ending screen

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,9 +1,60 @@
 extends Node3D
 
 var clock = 60 * 2
+var game_end = false
+var zooming_in = true
+
+func ending():
+	game_end = true
+	
+	$CanvasLayer/Control/ThePanel.visible = false 
+	$CanvasLayer/Control/PickUpPrompt.visible = false 
+	$CanvasLayer/Control/ThrowPrompt.visible = false 
+	$CanvasLayer/Control/Timer.visible = false
+	
+	$Camera3D.current = true
+	
+	$Drumroll.play()
+	
+	await get_tree().create_timer(4.242).timeout
+	
+	zooming_in = false
+	
+	# The reason we set both of them is because of jank
+	# Set them both or the failure screen looks weird
+	$CanvasLayer/Control/Success.scale = Vector2(0, 0)
+	$CanvasLayer/Control/Failure.scale = Vector2(0, 0)
+	
+	if false:
+		$CanvasLayer/Control/Success.visible = true
+	else:
+		$CanvasLayer/Control/Failure.visible = true
+		
+		$Failure.play()
+		
+		if randi_range(1, 5) == 1:
+			$YouStink.play()
 
 func _process(delta: float) -> void:
+	if game_end: 
+		if zooming_in:
+			$Camera3D.fov -= delta * 7
+		else:
+			$CanvasLayer/Control/Success.scale.x += (1 - $CanvasLayer/Control/Success.scale.x) / 2
+			$CanvasLayer/Control/Success.scale.y = $CanvasLayer/Control/Success.scale.x
+			
+			$CanvasLayer/Control/Failure.scale.x = $CanvasLayer/Control/Success.scale.x
+			$CanvasLayer/Control/Failure.scale.y = $CanvasLayer/Control/Success.scale.x
+			
+			pass
+		
+		return
+	
+	if clock <= 0:
+		ending()
+	
 	clock -= delta
+	
 	if roundi(clock)% 60 < 10: $CanvasLayer/Control/Timer.text = str(roundi(clock) / 60) + ":0" + str(roundi(clock) % 60)
 	else: $CanvasLayer/Control/Timer.text = str(roundi(clock) / 60) + ":" + str(roundi(clock) % 60)
 	
@@ -16,6 +67,8 @@ func _process(delta: float) -> void:
 
 func _on_collision_area_body_entered(body: Node3D) -> void:
 	body.queue_free()
+	
+	$Fix.play()
 	
 	if body.name == "Screws":
 		$Rocket/Screws.visible = true
