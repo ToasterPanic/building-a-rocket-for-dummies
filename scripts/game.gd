ff8a191fd8e10148b524b1497bc1e7aab7673f53
ToasterPanic <me@owouw.us>
2025-11-22 11:03:00 -0500

    Boxes that can be picked up and used to build rocket

---
+++ scripts/game.gd
@@ -0,0 +1,16 @@
+extends Node3D
+
+
+func _process(delta: float) -> void:
+	if $Player.held_object == null:
+		$CanvasLayer/Control/ThrowPrompt.visible = false
+		$CanvasLayer/Control/PickUpPrompt.visible = $Player/Camera3D/Interact.is_colliding()
+	else:
+		$CanvasLayer/Control/ThrowPrompt.visible = true
+		$CanvasLayer/Control/PickUpPrompt.visible = false
+
+func _on_collision_area_body_entered(body: Node3D) -> void:
+	body.queue_free()
+	
+	if body.name == "ExteriorWalls":
+		$Rocket/ExteriorWalls.visible = true
--- scripts/player.gd
+++ scripts/player.gd
@@ -5,6 +5,8 @@ var speed = 5
 var jump_speed = 5
 var mouse_sensitivity = 0.002
 
+var held_object = null
+
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
@@ -15,6 +17,25 @@ func _input(event):
 		$Camera3D.rotation.x = clampf($Camera3D.rotation.x, -deg_to_rad(70), deg_to_rad(70))
 		
 func _physics_process(delta):
+	if Input.is_action_just_pressed("throw"):
+		if held_object != null:
+			held_object.linear_velocity = $Camera3D.global_basis * Vector3(0, 0, -11)
+			held_object = null
+	elif Input.is_action_just_pressed("interact"):
+		if held_object != null: held_object = null
+		elif $Camera3D/Interact.is_colliding(): held_object = $Camera3D/Interact.get_collider()
+		
+	print(held_object)
+	
+	if held_object != null:
+		var target_pos = $Camera3D.global_transform.origin + ($Camera3D.global_basis * Vector3(0, 0, -2.5)) # 2.5 units in front of camera
+		var object_pos = held_object.global_transform.origin # Held object position
+		held_object.linear_velocity = (target_pos - object_pos) * 7 # Our desired position
+		
+		if held_object.global_position.distance_to($Camera3D.global_position) > 4:
+			held_object = null
+		
+		
 	velocity.y += -gravity * delta
 	var input = Input.get_vector("left", "right", "forward", "back")
 	var movement_dir = transform.basis * Vector3(input.x, 0, input.y)
