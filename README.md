db68845cdbb871bb3d5cb0035ec95610c68d4182
ToasterPanic <me@owouw.us>
2025-11-23 21:15:28 -0500

    Title screen panels, settings options

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # Building a Rocket for Dummies
 
 ## Credits
-
-Some sound effects by freesound_community on Pixabay
--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -7,10 +7,15 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	if mode == "menu":
+		$UI/ColorRect.mouse_filter = 2
 		$UI/ColorRect.color.a -= delta * 1
 		if $UI/ColorRect.color.a < 0:
 			$UI/ColorRect.color.a = 0
+			
+		$UI/Settings.visible = false
+		$UI/Credits.visible = false
 	else:
+		$UI/ColorRect.mouse_filter = 0
 		$UI/ColorRect.color.a += delta * 1
 		if $UI/ColorRect.color.a > 1:
 			$UI/ColorRect.color.a = 1
@@ -20,3 +25,16 @@ func _process(delta: float) -> void:
 
 func _on_play_pressed() -> void:
 	mode = "play"
+
+func _on_return_button_pressed() -> void:
+	mode = "menu"
+
+
+func _on_credits_pressed() -> void:
+	mode = "credits"
+	$UI/Credits.visible = true
+
+
+func _on_settings_pressed() -> void:
+	mode = "settings"
+	$UI/Settings.visible = true
